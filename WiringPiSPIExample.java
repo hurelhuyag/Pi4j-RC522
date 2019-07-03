0391ca51ed9e9800d5a14a44780e2a425abc95b4
Hurelhuyag <hurelhuyag@gmail.com>
2019-07-03 20:32:28 +0800

    chinese comments translated

--- WiringPiSPIExample.java
+++ WiringPiSPIExample.java
@@ -20,17 +20,17 @@ public class WiringPiSPIExample
         String strUID;
         byte sector=15,block=3;
 
-        //寻卡，获得序列号
+        //Find a card and get the serial number
         if(rc522.Request(RaspRC522.PICC_REQIDL, back_len) == rc522.MI_OK)
             System.out.println("Detecte card:"+back_len[0]);
-        //防冲撞
+        //Anti-collision
         if(rc522.AntiColl(tagid) != RaspRC522.MI_OK)
         {
             System.out.println("anticoll error");
             return;
         }
 
-        //显示序列号
+        //Display serial number
         strUID= Convert.bytesToHex(tagid);
         //System.out.println(strUID);
         //System.out.println("Card Read UID:" + tagid[0] + "," + tagid[1] + "," + tagid[2] + "," + tagid[3]);
@@ -39,13 +39,13 @@ public class WiringPiSPIExample
                 strUID.substring(4,6) + "," +
                 strUID.substring(6,8));
 
-        //默认密钥
+        //Default key
         byte[] defaultkey=new byte[]{(byte)0xFF,(byte)0xFF,(byte)0xFF,(byte)0xFF,(byte)0xFF,(byte)0xFF};
-        //Select the scanned tag，选中指定序列号的卡
+        //Select the scanned tag，Check the card with the specified serial number
         int size=rc522.Select_Tag(tagid);
         System.out.println("Size="+size);
 
-        //Authenticate,A密钥验证卡,
+        //Authenticate,A key verification card,
 
         status = rc522.Auth_Card(RaspRC522.PICC_AUTHENT1A, sector,block, defaultkey, tagid);
         if(status != RaspRC522.MI_OK)
@@ -54,7 +54,7 @@ public class WiringPiSPIExample
             return;
         }
 
-        //卡各扇区初始控制字FF078069，15扇区改为08778F69,A密钥改为330123
+        //The initial control word of each sector of the card is FF078069, the sector of 15 is changed to 08778F69, and the A key is changed to 330123.
         byte data[]=new byte[16];
         byte controlbytes[]=new byte[]{(byte)0x08,(byte)0x77,(byte)0x8f,(byte) 0x69};
         System.arraycopy(controlbytes,0,data,6,4);
